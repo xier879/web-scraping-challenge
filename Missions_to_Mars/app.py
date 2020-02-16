04ccfedcf381ffa32d5b0b75dbafe11a288b3d76
Xier Zhao <zhaox310@gmail.com>
2020-02-16 12:24:49 -0500

    edit app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,28 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 # Import our pymongo library, which lets us connect our Flask app to our Mongo database.
-import pymongo
+from flask_pymongo import PyMongo
+import scrape_mars
 
 # Create an instance of our Flask app.
-#classwork 12-3-08
+#classwork 12-3-09
 app = Flask(__name__)
-# Create connection variable
-conn = 'mongodb://localhost:27017'
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
-# Pass connection to the pymongo instance.
-client = pymongo.MongoClient(conn)
-# Connect to a database. Will create one if not already available.
-db = client.MarsMission_db
-collection=db.mars_data
 
-@app.route("/scrape")
-def scrape():
-    mars_scrape = scrape_mars.scrape()
-    collection.insert_one(mars_scrape)
 @app.route("/")
 def index():
-    mars = list(db.collection.find())
+    mars = mongo.db.mars.find_one()
     return render_template("index.html", mars = mars)
 
+@app.route("/scrape")
+def scraper():
+    mars = mongo.db.mars
+    mars_data= scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
